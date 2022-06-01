8292cef1f02329fc9b350b317d0e54fb82b9fca4
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2022-06-01 14:55:26 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -25,16 +25,7 @@ export default class HomeScreen extends Component {
 
   getPlanets = () => {
     const { url } = this.state;
-    axios
-      .get(url)
-      .then(response => {
-        return this.setState({
-          listData: response.data.data
-        });
-      })
-      .catch(error => {
-        Alert.alert(error.message);
-      });
+    
   };
 
   renderItem = ({ item, index }) => (
